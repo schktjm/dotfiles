a3fc32ad79c3e32279b0cdba1ebc15d3c25c68d6
schktjm <schktjm@gmail.com>
2022-09-03 18:12:51 +0900

    test

--- install_brew.sh
+++ install_brew.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/schktjm/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 brew bundle --file=.brewfile
 
